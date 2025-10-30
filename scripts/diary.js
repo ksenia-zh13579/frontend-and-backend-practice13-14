6354038d8fd8c6dd8b794824d0a4193fcb3a2425
ksenia-zh13579 <kseniazuzleva2395@gmail.com>
2025-10-30 18:32:24 +0300

    build contacts.html and fix forms

---
+++ scripts/diary.js
@@ -0,0 +1,28 @@
+let form = document.getElementsByTagName('form')[0];
+let skill = document.getElementById('inputSkill');
+let date = document.getElementById('inputDate');
+let stat = document.getElementById('selectStatus');
+
+let btnClose = document.getElementById('btnClose');
+btnClose.addEventListener('click', () => {
+    form.reset();
+});
+
+form.addEventListener('submit', (event) => {
+    if (form.checkValidity())
+    {
+        event.preventDefault();
+
+        dateValue = date.value.slice(8) + '.' + date.value.slice(5, 7) + '.' +date.value.slice(0, 4);
+        result = dateValue + ' - ' + skill.value + ' - ' + stat.value;
+        let list = document.getElementById('progressList');
+        let newSkillElem = document.createElement('li');
+        newSkillElem.innerHTML = result;
+        newSkillElem.classList.add('list-group-item');
+        list.prepend(newSkillElem); 
+
+        form.reset();
+        let modal = bootstrap.Modal.getInstance(document.getElementById('modalAddProgress'));
+        modal.hide();
+    }
+});
